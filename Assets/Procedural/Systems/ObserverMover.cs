6d05a8d2474b12d2878ecf6a975ceb135231423a
Sir Dudard <sir.dudard@theknightsofunity.com>
2021-09-29 21:11:29 +0200

    Camera controller added

---
+++ Assets/Procedural/Systems/ObserverMover.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Procedural
+{
+    public class ObserverMover : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed = 0;
+        [SerializeField]
+        private Camera observerCamera = null;
+
+        private const string horizontalAxis = "Horizontal";
+        private const string verticalAxis = "Vertical";
+
+        private void Update()
+        {
+            Vector3 forwardMoveVector = observerCamera.transform.forward * Input.GetAxis(verticalAxis) * speed;
+            Vector3 rightMoveVector = observerCamera.transform.right * Input.GetAxis(horizontalAxis) * speed;
+            transform.position += (forwardMoveVector + rightMoveVector) * Time.deltaTime;
+        }
+    }
+}
+
